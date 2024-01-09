96036459cd24170c268c947391a4c20193815f82
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-09 11:46:17 +0100

    work on main

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import streamlit as st
 
 # import pandas as pd
 
+img = "./assets/img/image.png"
+
+st.image(img, caption="image header")
+
 st.write(
     """
-# My first app
-Hello *world!*
+# Free Audible App
+Hello *user!*
 """
 )
+
+
+pos_url = "https://www.youtube.com/watch?v=EesQolfp9gQ,EesQolfp9gQ"
+
+url = st.text_input("Youtube url", pos_url)
+st.write("video url is", url)
+
 
 # df = pd.read_csv("my_data.csv")
 # st.line_chart(df)
