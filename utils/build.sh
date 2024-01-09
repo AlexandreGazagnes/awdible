308895d6c2c7ad7980bcd63677299fc461f522b2
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-09 11:45:47 +0100

    delete plain out

--- utils/build.sh
+++ utils/build.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 # build docker image
-docker build -f ./Dockerfile -t audible:latest . 
+# --progress=plain
+docker build -f ./Dockerfile  -t audible:latest . 
 
 # run docker image
-sudo docker run  -p 8501:8501 -d --env-file .env/.env.dev audible:latest
+# sudo docker run  -p 8501:8501 -d --env-file .env/.env.dev audible:latest
+sudo docker run  -p 8501:8501 --env-file .env/.env.dev audible:latest
