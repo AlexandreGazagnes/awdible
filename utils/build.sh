d0b28b86450239b45103fad9bc6bebd4f09c01c2
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-09 11:11:03 +0100

    upade build

--- utils/build.sh
+++ utils/build.sh
@@ -4,4 +4,4 @@
 docker build -f ./Dockerfile -t audible:latest . 
 
 # run docker image
-sudo docker run  -p 8501:8501 -d --env-file .env.dev audible:latest
+sudo docker run  -p 8501:8501 -d --env-file .env/.env.dev audible:latest
