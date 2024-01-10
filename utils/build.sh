3d868db6dc140e62b32db96100acebb27c779562
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-10 11:14:56 +0100

    clean build

--- utils/build.sh
+++ utils/build.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 # build docker image
-# --progress=plain
+# --progress=plain for showing progress bar
 docker build -f ./Dockerfile  -t audible:latest . 
 
 # run docker image
-# sudo docker run  -p 8501:8501 -d --env-file .env/.env.dev audible:latest
+# -d for detached mode
 sudo docker run  -p 8501:8501 --env-file .env/.env.dev audible:latest
 
-
+# if needed to run bash inside container
 # docker exec -it <container-name> /bin/bash
