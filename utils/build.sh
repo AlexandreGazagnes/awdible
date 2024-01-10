e6682bcd4d939a666710b64bb754cdaae9c1d3a6
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-10 11:06:05 +0100

    update  build

--- utils/build.sh
+++ utils/build.sh
@@ -7,3 +7,6 @@ docker build -f ./Dockerfile  -t audible:latest .
 # run docker image
 # sudo docker run  -p 8501:8501 -d --env-file .env/.env.dev audible:latest
 sudo docker run  -p 8501:8501 --env-file .env/.env.dev audible:latest
+
+
+# docker exec -it <container-name> /bin/bash
