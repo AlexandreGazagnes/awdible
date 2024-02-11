9d22370b9c928ea3b96f3963c58b959c03ee3255
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-11 18:02:57 +0100

    update utils

---
+++ .utils/kill_env.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+rm -rf .venv
+rm -rf poetry.lock
+# python3.11 -m poetry install
--- .utils/pytest.sh
+++ .utils/pytest.sh
@@ -1,3 +1,7 @@
-#! /bin/bash
+#! /bin/sh
 
-pytest -vvx --capture=tee-sys --log-cli-level=INFO tests/
+
+rm -f ./docs/assets/img/cov.svg
+# rm -f .coverage
+.venv/bin/coverage run -m pytest -vvx --capture=tee-sys --log-cli-level=INFO tests/
+.venv/bin/coverage-badge -fo ./docs/assets/img/cov.svg
