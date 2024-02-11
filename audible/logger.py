7022093c25a55deaa3154aeafbc7642489a6ac6b
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-11 22:36:58 +0100

    helpers and logger

---
+++ audible/logger.py
@@ -0,0 +1,16 @@
+import logging
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    return logger
+
+
+logger = get_logger(__name__)
