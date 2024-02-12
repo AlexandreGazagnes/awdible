1b86ac82c6cb284802f5387765f99eb0274b91cf
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-12 21:27:24 +0100

    update test script

--- .utils/pytest.sh
+++ .utils/pytest.sh
@@ -3,5 +3,5 @@
 
 rm -f ./docs/assets/img/cov.svg
 # rm -f .coverage
-.venv/bin/coverage run -m pytest -vvx --capture=tee-sys --log-cli-level=INFO tests/
+.venv/bin/coverage run -m pytest -vvx --capture=tee-sys --log-cli-level=INFO tests/integration/
 .venv/bin/coverage-badge -fo ./docs/assets/img/cov.svg
