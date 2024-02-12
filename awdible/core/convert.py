50b0a8160dc402cd8b75cb140234b3d12464d954
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-12 21:13:28 +0100

    clean files

--- awdible/core/convert.py
+++ awdible/core/convert.py
@@ -19,7 +19,11 @@ class Convert:
         # logger.debug(f"Destination: {dest}")
 
         # the command
-        cmd = {"q:a": 0, "map": "a"}
+        cmd = {
+            "q:a": 0,
+            "map": "a",
+            "loglevel": "quiet",
+        }
 
         # do the conversion
         ffmpeg.input(src).output(dest, **cmd).run()
