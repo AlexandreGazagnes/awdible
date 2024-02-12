5e8108d096ad4a5be2882cbeac01ab61f250ddc0
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-12 01:27:39 +0100

    rework unit tets

---
+++ tests/unit/core/test_search.py
@@ -0,0 +1,27 @@
+from audible.core.search import Search
+
+from audible.logger import logger
+
+
+QUERY = "jo l'rigolo"
+
+
+class TestSearch:
+
+    def test_find(self) -> None:
+        """Test the find method"""
+
+        result = Search.find(QUERY)
+
+        # logger.info(result[:1000])
+        assert result is not None
+        # logger.info(result)
+
+    def test_parse(self) -> None:
+        """Test the parse method"""
+
+        json = Search.find(QUERY, context="fr")
+
+        result = Search.parse(json)
+
+        logger.info(result)
