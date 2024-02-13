c89928df79afd224f2b82b57994ec88dff0c1c2b
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-13 16:33:14 +0100

    manage destination in file

--- awdible/gui/front.py
+++ awdible/gui/front.py
@@ -6,9 +6,22 @@ import streamlit as st
 
 from awdible.core.awdible import Awdible
 
+from awdible.helpers import asciize
+
 DEFAULT_VIDEO_URL = "https://www.youtube.com/watch?v=9diaThxYnKA"
 DEFAULT_VIDEO_ID = "V62oKsHdsLU"
 DEFAULT_VIDEO_QUERY = "jo l'rigolo"
+
+
+def user_out(out, dest="./tmp/"):
+    """User output"""
+
+    out = out.replace(dest, "")
+    out = asciize(out)
+    out = out.removeprefix(".")
+
+    return out
+
 
 # header image
 img = "./docs/assets/img/image.png"
@@ -34,10 +47,14 @@ if st.button("Submit"):
     # init the Awdible class
     awdible = Awdible(video=url, test_mode=True, streamlit=True)
 
+    awdible.dest
+
     # do run
     outs = awdible.run()
     out = outs[0] if isinstance(outs, list) else outs
 
     # deliver the file
     with open(out, "rb") as f:
-        st.download_button("Download", f, file_name=out)  # mime="audio/mp4"
+        st.download_button(
+            "Download", f, file_name=user_out(out, awdible.dest)
+        )  # mime="audio/mp4"
