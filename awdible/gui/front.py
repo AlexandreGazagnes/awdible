c132d8a0a964eb28586513519b0e476944194593
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-13 16:02:19 +0100

    update out

--- awdible/gui/front.py
+++ awdible/gui/front.py
@@ -13,7 +13,7 @@ st.image(img)  # caption="Thanks to DALL-E for the image ;) "
 
 
 # title
-st.title("The Best Free Awdible App Ever Made")
+st.title("Awdible - Just the best free version of audible")
 st.write(
     """
 Just paste the url of the video you want to download and click the button.
@@ -27,7 +27,14 @@ url = st.text_input("Youtube url", pos_url)
 
 # Button
 if st.button("Submit"):
-    awdible = Awdible(video=url, test_mode=False, streamlit=True)
-    out = awdible.run()
+
+    # init the Awdible class
+    awdible = Awdible(video=url, test_mode=True, streamlit=True)
+
+    # do run
+    outs = awdible.run()
+    out = outs[0] if isinstance(outs, list) else outs
+
+    # deliver the file
     with open(out, "rb") as f:
         st.download_button("Download", f, file_name=out)  # mime="audio/mp4"
