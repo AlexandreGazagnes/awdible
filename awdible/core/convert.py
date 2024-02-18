ca0690281114ee38fbee918601d613695ede2af6
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-18 20:23:31 +0100

    add new os cmd

--- awdible/core/convert.py
+++ awdible/core/convert.py
@@ -4,15 +4,22 @@ Convert the video to mp3
 
 import os
 
-import ffmpeg
+# import ffmpeg
 
 from awdible.logger import logger
 
 
 class Convert:
     @classmethod
-    def to_mp3(cls, src: str) -> str:
+    def to_mp3(
+        cls,
+        src: str,
+        is_ffmpeg_installed: bool = True,
+    ) -> str:
         """Convert the video to mp3"""
+
+        if not is_ffmpeg_installed:
+            logger.warning("ffmpeg is not installed")
 
         logger.debug(f"Converting {src} to mp3")
 
@@ -22,15 +29,20 @@ class Convert:
 
         # logger.debug(f"Destination: {dest}")
 
+        ############## Old method ################
         # the command
-        cmd = {
-            "q:a": 0,
-            "map": "a",
-            "loglevel": "quiet",
-        }
+        # cmd = {
+        #     "q:a": 0,
+        #     "map": "a",
+        #     "loglevel": "quiet",
+        # }
 
         # do the conversion
-        ffmpeg.input(src).output(dest, **cmd).run()
+        # ffmpeg.input(src).output(dest, **cmd).run()
+
+        ############## New method ################
+        cmd = f"ffmpeg -i {src} -q:a 0 -map a {dest} -y"
+        os.system(cmd)
 
         # del  old file
         os.remove(src)
