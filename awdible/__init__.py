a9b06a76d82e148852d23b8645d817dcfce7ea27
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-02-19 23:15:04 +0100

    fix versionning

--- awdible/__init__.py
+++ awdible/__init__.py
@@ -2,7 +2,7 @@
 Awdible is a Python library for working as a Audible like.
 """
 
-__VERSION__ = "0.2.4"
+__VERSION__ = "0.2.5"
 __DESCRIPTION__ = "Awdible - Just the best free version of audible"
 __AUTHOR__ = "Alexandre Gazagnes"
 
